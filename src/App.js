2971cddab8858af74bfc665e05bd1601104e2964
KRATIK_BOHRA <kratikbohra5@gmail.com>
2023-10-09 01:59:02 +0530

    Changed the name of heading from Learning Todo to Tasklist-kratik

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ const App = () => {
                     d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
                   />
                 </svg>
-                <p className=" ml-8"> Learning TODO</p>
+                <p className=" ml-8">TASK LIST - KRATIK</p>
               </h1>
             </div>
 
